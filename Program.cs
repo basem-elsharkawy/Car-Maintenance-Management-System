ce3af93e12a4b9cbbf63932da4a0ce1e687dee7d
Basem ElSharkawy <basem.waleed006@gmail.com>
2025-12-11 01:49:44 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,13 @@
 
+using System;  // ضروري علشان نستخدم Console
+
+namespace CarMaintenanceSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
