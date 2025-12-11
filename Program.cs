51c1646841a9f534e545cfe8ef6095a79c856d33
Basem ElSharkawy <basem.waleed006@gmail.com>
2025-12-11 02:26:02 +0200

    Add VehicleCategory, SUV and Sedan Classes
    add the classes and keep checkFuelEfficiency() and  checkOffRoadCapability() till final edit

--- Program.cs
+++ Program.cs
@@ -2,6 +2,51 @@ using System;
 
 namespace CarMaintenanceSystem
 {
+    // Start Vehicle Category Class
+     class VehicleCategory
+    {
+        private string categoryID;
+        private string name;
+        private string description;
+
+        public string CategoryID { get { return categoryID; } }
+        public string Name { get { return name; } }
+        public string Description { get { return description; } }
+
+        public VehicleCategory(string categoryID, string name, string description)
+        {
+            this.categoryID = categoryID;
+            this.name = name;
+            this.description = description;
+        }
+     // Ends Vehicle Category Class
+
+    // Start Sedan Class 
+     class Sedan : VehicleCategory
+    {
+        public Sedan(string categoryID, string name, string description)
+            : base(categoryID, name, description) { }
+
+        public void checkFuelEfficiency()
+        {
+           // Console.WriteLine("Checking fuel efficiency for sedan...");  //Waiting untill final decision
+        }
+    }
+    // End Sedan Class
+
+    // Start SUV Class
+     class SUV : VehicleCategory
+    {
+        public SUV(string categoryID, string name, string description)
+            : base(categoryID, name, description) { }
+
+        public void checkOffRoadCapability()
+        {
+          //  Console.WriteLine("Checking off-road capability for SUV...");   // //Waiting untill final decision
+        }
+    }
+    // End SUV Class
+    }
     class Program
     {
         static void Main(string[] args)
